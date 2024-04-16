65a7be4da9de3c33c2cf2c34d78c51140fcff0d2
adewale <temade91@gmail.com>
2024-04-16 10:10:52 +0100

    new changes made

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+<<<<<<< HEAD
 
 this is my pyton projects 
+=======
+this is my test application page
+this is my test application page
+this is my test application page
+this is my test application page
+
+>>>>>>> 40b6081316e19361a9b7a01f31e9542a1b6caccc
